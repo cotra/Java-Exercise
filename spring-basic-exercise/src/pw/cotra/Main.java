81d8a45e01ffe3d13cc092f76a935e1a41c1d8d5
lin <mr03web@foxmail.com>
2019-08-02 18:07:01 +0800

    bean

--- spring-basic-exercise/src/pw/cotra/Main.java
+++ spring-basic-exercise/src/pw/cotra/Main.java
@@ -1,12 +1,13 @@
 package pw.cotra;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pw.cotra.editor.TextEditor;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorldPo");
-        obj.getMessage();
+        TextEditor editor = (TextEditor) context.getBean("TextEditor");
+        editor.spellCheck();
     }
 }
